6d4697c9ace0fddd88922266646d9f29db6331db
Pathurs <pathurs123@gmail.com>
2018-08-26 15:24:45 +1000

    fix(lint): tslint errors

--- src/general/get-own-property-descriptors.ts
+++ src/general/get-own-property-descriptors.ts
@@ -1,7 +1,7 @@
 export function getOwnPropertyDescriptors<T>(object: T): PropertyDescriptorMap {
     const propertyDescriptorMap: PropertyDescriptorMap = {};
 
-    const propertyNames = Object.getOwnPropertyNames(object).forEach(key => {
+    Object.getOwnPropertyNames(object).forEach(key => {
         propertyDescriptorMap[key] = <PropertyDescriptor>Object.getOwnPropertyDescriptor(object, key);
     });
 
