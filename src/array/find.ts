85b9a67d24745889754222d31c0bd0d376a2c93b
Patrick Stanford <contact@pathurs.com>
2018-10-28 21:24:42 +1100

    feat: upgrade TypeScript version to 3.1.3
    BREAKING CHANGE: TypeScript version 2.7.2 changed to 3.1.3

--- src/array/find.ts
+++ src/array/find.ts
@@ -3,7 +3,8 @@ import { Finder } from '../types/finder';
 export function find<T>(array: T[], item: T): T | undefined;
 export function find<T>(array: T[], finder: Finder<T>): T | undefined;
 export function find<T>(array: T[], itemOrFinder: T | Finder<T>) {
-    const finder: Finder<T> = typeof itemOrFinder === 'function' ? itemOrFinder : item => item === itemOrFinder;
+    const finder: Finder<T> =
+        typeof itemOrFinder === 'function' ? <Finder<T>>itemOrFinder : item => item === itemOrFinder;
 
     for (let i = 0; i < array.length; i++) {
         if (finder(array[i], i, array)) {
