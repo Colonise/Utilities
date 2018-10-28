f1bac348c008c62ed9ccf1f1fe29428c24f200d5
Pathurs <pathurs123@gmail.com>
2018-10-28 22:03:01 +1100

    fix: replace tslint with TypeScript unused variable

--- src/general/copy-to.spec.ts
+++ src/general/copy-to.spec.ts
@@ -4,6 +4,7 @@ import { IgnoreTest, Test, TestFixture } from 'alsatian';
 export class CopyToTests {
     @IgnoreTest('TODO')
     @Test('copyTo()')
+    // @ts-ignore: Allow unused parameter for ignored test
     public copyTo1<T>(array: T[], expected: T, item: T) {
         //
     }
--- src/general/copy.spec.ts
+++ src/general/copy.spec.ts
@@ -4,6 +4,7 @@ import { IgnoreTest, Test, TestFixture } from 'alsatian';
 export class CopyTests {
     @IgnoreTest('TODO')
     @Test('copy()')
+    // @ts-ignore: Allow unused parameter for ignored test
     public copy1<T>(array: T[], expected: T, item: T) {
         //
     }
