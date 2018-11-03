a982824099d6c9dc23be5c3eee37f1cfaf845ecd
Patrick Stanford <contact@pathurs.com>
2018-11-04 10:46:57 +1100

    fix(findIndex): replace findIndex with finIndex and findIndexBy

--- src/array/find-index.ts
+++ src/array/find-index.ts
@@ -1,16 +1,6 @@
-import { Finder } from '../types/finder';
+import { findIndexBy } from './find-index-by';
 
 export function findIndex<T>(array: T[], item: T): number;
-export function findIndex<T>(array: T[], finder: Finder<T>): number;
-export function findIndex<T>(array: T[], itemOrFinder: T | Finder<T>) {
-    const finder: Finder<T> =
-        typeof itemOrFinder === 'function' ? <Finder<T>>itemOrFinder : item => item === itemOrFinder;
-
-    for (let i = 0; i < array.length; i++) {
-        if (finder(array[i], i, array)) {
-            return i;
-        }
-    }
-
-    return -1;
+export function findIndex<T>(array: T[], itemToFind: T): number {
+    return findIndexBy(array, item => item === itemToFind);
 }
--- src/array/index.ts
+++ src/array/index.ts
@@ -1,5 +1,6 @@
 export * from './find';
 export * from './find-by';
 export * from './find-index';
+export * from './find-index-by';
 export * from './insert';
 export * from './remove';
