c69b98a69636a129daf0dcc654c9f0c52ca9a0db
Patrick Stanford <contact@pathurs.com>
2018-11-03 21:46:16 +1100

    fix(extract): fix extract's intellisense

--- src/general/extract.ts
+++ src/general/extract.ts
@@ -1,124 +1,168 @@
-import { Unknown } from '../types/unknown';
+import { RecursivelyDefined } from '../types/defined';
 
-export function extract<T, P extends keyof T>(object: T, property: P): T[P] | undefined;
-export function extract<T, P1 extends keyof T, P2 extends keyof T[P1]>(
-    object: T,
-    property1: P1,
-    property2: P2
-): T[P1][P2] | undefined;
-export function extract<T, P1 extends keyof T, P2 extends keyof T[P1], P3 extends keyof T[P1][P2]>(
-    object: T,
-    property1: P1,
-    property2: P2,
-    property3: P3
-): T[P1][P2][P3] | undefined;
+export function extract<TObject>(object: TObject): RecursivelyDefined<TObject> | undefined;
+export function extract<TObject, TProperty extends keyof RecursivelyDefined<TObject>>(
+    object: TObject,
+    property: TProperty
+): RecursivelyDefined<TObject>[TProperty] | undefined;
 export function extract<
-    T,
-    P1 extends keyof T,
-    P2 extends keyof T[P1],
-    P3 extends keyof T[P1][P2],
-    P4 extends keyof T[P1][P2][P3]
->(object: T, property1: P1, property2: P2, property3: P3, property4: P4): T[P1][P2][P3][P4] | undefined;
-export function extract<
-    T,
-    P1 extends keyof T,
-    P2 extends keyof T[P1],
-    P3 extends keyof T[P1][P2],
-    P4 extends keyof T[P1][P2][P3],
-    P5 extends keyof T[P1][P2][P3][P4]
+    TObject,
+    TProperty1 extends keyof RecursivelyDefined<TObject>,
+    TProperty2 extends keyof RecursivelyDefined<TObject>[TProperty1]
 >(
-    object: T,
-    property1: P1,
-    property2: P2,
-    property3: P3,
-    property4: P4,
-    property5: P5
-): T[P1][P2][P3][P4][P5] | undefined;
+    object: TObject,
+    property1: TProperty1,
+    property2: TProperty2
+): RecursivelyDefined<TObject>[TProperty1][TProperty2] | undefined;
 export function extract<
-    T,
-    P1 extends keyof T,
-    P2 extends keyof T[P1],
-    P3 extends keyof T[P1][P2],
-    P4 extends keyof T[P1][P2][P3],
-    P5 extends keyof T[P1][P2][P3][P4],
-    P6 extends keyof T[P1][P2][P3][P4][P5]
+    TObject,
+    TProperty1 extends keyof RecursivelyDefined<TObject>,
+    TProperty2 extends keyof RecursivelyDefined<TObject>[TProperty1],
+    TProperty3 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2]
 >(
-    object: T,
-    property1: P1,
-    property2: P2,
-    property3: P3,
-    property4: P4,
-    property5: P5,
-    property6: P6
-): T[P1][P2][P3][P4][P5][P6] | undefined;
+    object: TObject,
+    property1: TProperty1,
+    property2: TProperty2,
+    property3: TProperty3
+): RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3] | undefined;
 export function extract<
-    T,
-    P1 extends keyof T,
-    P2 extends keyof T[P1],
-    P3 extends keyof T[P1][P2],
-    P4 extends keyof T[P1][P2][P3],
-    P5 extends keyof T[P1][P2][P3][P4],
-    P6 extends keyof T[P1][P2][P3][P4][P5],
-    P7 extends keyof T[P1][P2][P3][P4][P5][P6]
+    TObject,
+    TProperty1 extends keyof RecursivelyDefined<TObject>,
+    TProperty2 extends keyof RecursivelyDefined<TObject>[TProperty1],
+    TProperty3 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2],
+    TProperty4 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3]
 >(
-    object: T,
-    property1: P1,
-    property2: P2,
-    property3: P3,
-    property4: P4,
-    property5: P5,
-    property6: P6,
-    property7: P7
-): T[P1][P2][P3][P4][P5][P6][P7] | undefined;
+    object: TObject,
+    property1: TProperty1,
+    property2: TProperty2,
+    property3: TProperty3,
+    property4: TProperty4
+): RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3][TProperty4] | undefined;
 export function extract<
-    T,
-    P1 extends keyof T,
-    P2 extends keyof T[P1],
-    P3 extends keyof T[P1][P2],
-    P4 extends keyof T[P1][P2][P3],
-    P5 extends keyof T[P1][P2][P3][P4],
-    P6 extends keyof T[P1][P2][P3][P4][P5],
-    P7 extends keyof T[P1][P2][P3][P4][P5][P6],
-    P8 extends keyof T[P1][P2][P3][P4][P5][P6][P7]
+    TObject,
+    TProperty1 extends keyof RecursivelyDefined<TObject>,
+    TProperty2 extends keyof RecursivelyDefined<TObject>[TProperty1],
+    TProperty3 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2],
+    TProperty4 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3],
+    TProperty5 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3][TProperty4]
 >(
-    object: T,
-    property1: P1,
-    property2: P2,
-    property3: P3,
-    property4: P4,
-    property5: P5,
-    property6: P6,
-    property7: P7,
-    property8: P8
-): T[P1][P2][P3][P4][P5][P6][P7][P8] | undefined;
+    object: TObject,
+    property1: TProperty1,
+    property2: TProperty2,
+    property3: TProperty3,
+    property4: TProperty4,
+    property5: TProperty5
+): RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3][TProperty4][TProperty5] | undefined;
 export function extract<
-    T,
-    P1 extends keyof T,
-    P2 extends keyof T[P1],
-    P3 extends keyof T[P1][P2],
-    P4 extends keyof T[P1][P2][P3],
-    P5 extends keyof T[P1][P2][P3][P4],
-    P6 extends keyof T[P1][P2][P3][P4][P5],
-    P7 extends keyof T[P1][P2][P3][P4][P5][P6],
-    P8 extends keyof T[P1][P2][P3][P4][P5][P6][P7],
-    P9 extends keyof T[P1][P2][P3][P4][P5][P6][P7][P8]
+    TObject,
+    TProperty1 extends keyof RecursivelyDefined<TObject>,
+    TProperty2 extends keyof RecursivelyDefined<TObject>[TProperty1],
+    TProperty3 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2],
+    TProperty4 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3],
+    TProperty5 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3][TProperty4],
+    TProperty6 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3][TProperty4][TProperty5]
 >(
-    object: T,
-    property1: P1,
-    property2: P2,
-    property3: P3,
-    property4: P4,
-    property5: P5,
-    property6: P6,
-    property7: P7,
-    property8: P8,
-    property9: P9
-): T[P1][P2][P3][P4][P5][P6][P7][P8][P9] | undefined;
-export function extract<T>(object: T, ...properties: string[]): Unknown {
+    object: TObject,
+    property1: TProperty1,
+    property2: TProperty2,
+    property3: TProperty3,
+    property4: TProperty4,
+    property5: TProperty5,
+    property6: TProperty6
+): RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3][TProperty4][TProperty5][TProperty6] | undefined;
+export function extract<
+    TObject,
+    TProperty1 extends keyof RecursivelyDefined<TObject>,
+    TProperty2 extends keyof RecursivelyDefined<TObject>[TProperty1],
+    TProperty3 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2],
+    TProperty4 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3],
+    TProperty5 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3][TProperty4],
+    TProperty6 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3][TProperty4][TProperty5],
+    TProperty7 extends keyof RecursivelyDefined<
+        TObject
+    >[TProperty1][TProperty2][TProperty3][TProperty4][TProperty5][TProperty6]
+>(
+    object: TObject,
+    property1: TProperty1,
+    property2: TProperty2,
+    property3: TProperty3,
+    property4: TProperty4,
+    property5: TProperty5,
+    property6: TProperty6,
+    property7: TProperty7
+):
+    | RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3][TProperty4][TProperty5][TProperty6][TProperty7]
+    | undefined;
+export function extract<
+    TObject,
+    TProperty1 extends keyof RecursivelyDefined<TObject>,
+    TProperty2 extends keyof RecursivelyDefined<TObject>[TProperty1],
+    TProperty3 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2],
+    TProperty4 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3],
+    TProperty5 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3][TProperty4],
+    TProperty6 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3][TProperty4][TProperty5],
+    TProperty7 extends keyof RecursivelyDefined<
+        TObject
+    >[TProperty1][TProperty2][TProperty3][TProperty4][TProperty5][TProperty6],
+    TProperty8 extends keyof RecursivelyDefined<
+        TObject
+    >[TProperty1][TProperty2][TProperty3][TProperty4][TProperty5][TProperty6][TProperty7]
+>(
+    object: TObject,
+    property1: TProperty1,
+    property2: TProperty2,
+    property3: TProperty3,
+    property4: TProperty4,
+    property5: TProperty5,
+    property6: TProperty6,
+    property7: TProperty7,
+    property8: TProperty8
+):
+    | RecursivelyDefined<
+          TObject
+      >[TProperty1][TProperty2][TProperty3][TProperty4][TProperty5][TProperty6][TProperty7][TProperty8]
+    | undefined;
+export function extract<
+    TObject,
+    TProperty1 extends keyof RecursivelyDefined<TObject>,
+    TProperty2 extends keyof RecursivelyDefined<TObject>[TProperty1],
+    TProperty3 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2],
+    TProperty4 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3],
+    TProperty5 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3][TProperty4],
+    TProperty6 extends keyof RecursivelyDefined<TObject>[TProperty1][TProperty2][TProperty3][TProperty4][TProperty5],
+    TProperty7 extends keyof RecursivelyDefined<
+        TObject
+    >[TProperty1][TProperty2][TProperty3][TProperty4][TProperty5][TProperty6],
+    TProperty8 extends keyof RecursivelyDefined<
+        TObject
+    >[TProperty1][TProperty2][TProperty3][TProperty4][TProperty5][TProperty6][TProperty7],
+    TProperty9 extends keyof RecursivelyDefined<
+        TObject
+    >[TProperty1][TProperty2][TProperty3][TProperty4][TProperty5][TProperty6][TProperty7][TProperty8]
+>(
+    object: TObject,
+    property1: TProperty1,
+    property2: TProperty2,
+    property3: TProperty3,
+    property4: TProperty4,
+    property5: TProperty5,
+    property6: TProperty6,
+    property7: TProperty7,
+    property8: TProperty8,
+    property9: TProperty9
+):
+    | RecursivelyDefined<
+          TObject
+      >[TProperty1][TProperty2][TProperty3][TProperty4][TProperty5][TProperty6][TProperty7][TProperty8][TProperty9]
+    | undefined;
+export function extract(object: unknown, ...properties: string[]): unknown {
+    // Allow any because to allow easy non-null and non-undefined coalescing
     // tslint:disable-next-line:no-any
-    let result: any = object;
+    let result = <any>object;
 
-    for (let i = 0; i < properties.length; i++) {
+    const length = properties.length;
+
+    for (let i = 0; i < length; i++) {
         const property = properties[i];
         const current = result[property];
 
