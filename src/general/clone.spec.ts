2ce02b5e9e2f5eb060d80b18f0b4991f9c33c2ad
Patrick Stanford <contact@pathurs.com>
2018-11-04 18:58:45 +1100

    test(clone): add test case for class instance cloning

--- src/general/clone.spec.ts
+++ src/general/clone.spec.ts
@@ -1,14 +1,27 @@
 import { Expect, Test, TestCase, TestFixture } from 'alsatian';
+import { Constructor } from '../types';
 import { clone } from './clone';
+
+class TestingClass {
+    public c = this.a + this.b;
+
+    public constructor(public a: number, public b: number) {}
+
+    public d() {
+        return this.a + this.b + this.c;
+    }
+}
 
 @TestFixture('clone() Tests')
 export class CloneTests {
-    @TestCase(['a', 'b', 'c', 'd', 'e'], ['a', 'b', 'c', 'd', 'e'])
     @TestCase({ a: 1, b: 2, c: 3 }, { a: 1, b: 2, c: 3 })
+    @TestCase(['a', 'b', 'c', 'd', 'e'], ['a', 'b', 'c', 'd', 'e'])
+    @TestCase(new TestingClass(1, 2), new TestingClass(1, 2))
     @Test('clone<T>(object: T) should clone an object')
     public clone1<T>(object: T, expected: T) {
         const actual = clone(object);
 
         Expect(actual).toEqual(expected);
+        Expect(actual instanceof (<T & { constructor: Constructor<T> }>expected).constructor).toBe(true);
     }
 }
