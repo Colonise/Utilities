42fa5a6083723e66aca30f24bc55698a6e8a66c9
Patrick Stanford <contact@pathurs.com>
2018-11-06 11:36:49 +1100

    fix(Constructor):  make Constructor extend Function

--- src/types/constructor.d.ts
+++ src/types/constructor.d.ts
@@ -1,5 +1,5 @@
-export interface Constructor<T> {
-    prototype?: T;
+export interface Constructor<T = unknown> extends Function {
+    prototype: T;
     name?: string;
     // Allow any because we cannot know what a constructor's arguments are
     // tslint:disable-next-line:no-any
