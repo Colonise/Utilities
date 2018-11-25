031983368177a26547514f158ebcdbff07fe7b50
Patrick Stanford <contact@pathurs.com>
2018-11-25 17:11:27 +1100

    fix(remove): remove remove(array, items) and remove(array, index)

--- src/array/remove.spec.ts
+++ src/array/remove.spec.ts
@@ -1,9 +1,8 @@
 import { Expect, Test, TestCase, TestFixture } from 'alsatian';
 import { remove } from './remove';
 
-@TestFixture('remove Tests')
+@TestFixture('remove() Tests')
 export class RemoveTests {
-
     @TestCase(['a', 'b', 'c', 'd', 'e'], ['a', 'b', 'd', 'e'], 2)
     @Test('remove<T>(array: T[], item: T) should remove an item from an array')
     public remove1<T>(array: T[], expected: T[], item: T) {
@@ -11,22 +10,4 @@ export class RemoveTests {
 
         Expect(actual).toEqual(expected);
     }
-
-    @TestCase(['a', 'b', 'c', 'd', 'e'], ['a', 'b', 'e'], ['c', 'd'])
-    @Test('remove<T>(array: T[], items: T[]) should remove an array of items from an array')
-    public remove2<T>(array: T[], expected: T[], items: T[]) {
-        const actual = remove(array, items);
-
-        Expect(actual).toEqual(expected);
-    }
-
-    @TestCase(['a', 'b', 'c', 'd', 'e'], ['a', 'b', 'e'], 2, 2)
-    @Test(
-        'remove<T>(array: T[], index: number, count?: number) should remove a count of items from an array by an index'
-    )
-    public remove3<T>(array: T[], expected: T[], index: number, count: number) {
-        const actual = remove(array, index, count);
-
-        Expect(actual).toEqual(expected);
-}
 }
--- src/array/remove.ts
+++ src/array/remove.ts
@@ -1,18 +1,5 @@
-import { isArray } from '../types';
+import { removeAt } from './remove-at';
 
-export function remove<T>(array: T[], item: T): T[];
-export function remove<T>(array: T[], items: T[]): T[];
-export function remove<T>(array: T[], index: number, count?: number): T[];
-export function remove<T>(array: T[], indexOrItemOrItems: number | T | T[], count: number = 1) {
-    if (typeof indexOrItemOrItems === 'number') {
-        array.splice(indexOrItemOrItems, count);
-    } else if (isArray(indexOrItemOrItems)) {
-        indexOrItemOrItems.forEach(item => remove(array, item));
-    } else {
-        const index = array.indexOf(indexOrItemOrItems);
-
-        array.splice(index, 1);
-    }
-
-    return array;
+export function remove<T>(array: T[], item: T): T[] {
+    return removeAt(array, array.indexOf(item));
 }
