e054573a885e39beb1f1e3aa5f66926d4aa2ad6c
Pathurs <pathurs123@gmail.com>
2018-11-25 17:19:41 +1100

    chore: fix test fixture descriptions

--- src/array/find-by.spec.ts
+++ src/array/find-by.spec.ts
@@ -2,7 +2,7 @@ import { Expect, Test, TestCase, TestFixture } from 'alsatian';
 import { Finder } from '../types';
 import { findBy } from './find-by';
 
-@TestFixture('findBy Tests')
+@TestFixture('findBy() Tests')
 export class FindByTests {
     @TestCase(['a', 'b', 'c', 'd', 'e'], undefined, <Finder<string>>(item => item === 'f'))
     @Test('findBy<T>(array: T[], finder: Finder<T>) should use a finder to return an item')
