33f568512a3140a8649d7923c07675979bdba21e
Patrick Stanford <PStanford@janison.com.au>
2019-01-23 11:43:02 +1100

    fix(Constructor): remove `name` property to fix es5 and es6+ types

--- src/types/constructor.d.ts
+++ src/types/constructor.d.ts
@@ -1,5 +1,4 @@
 export interface Constructor<T = unknown> extends Function {
-    name?: string;
     prototype: T;
     // Allow any because we cannot know what a constructor's arguments are
     // tslint:disable-next-line:no-any
--- src/types/type-guards/is-object-tag.ts
+++ src/types/type-guards/is-object-tag.ts
@@ -2,7 +2,7 @@ import { Constructor } from '../constructor';
 
 export function isObjectTag<T>(object: T, tag: string): boolean;
 export function isObjectTag<T>(object: T, constructor: Constructor<T>): boolean;
-export function isObjectTag<T>(object: T, tagOrConstructor: string | Constructor<T>): boolean {
+export function isObjectTag<T>(object: T, tagOrConstructor: string | Constructor<T> & { name?: string }): boolean {
     const name = typeof tagOrConstructor === 'function' ? tagOrConstructor.name : tagOrConstructor;
 
     return Object.prototype.toString.call(object) === `[object ${name}]`;
