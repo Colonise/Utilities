9aa11a65cd41e8a54c713f097b76f72bb46d8629
Patrick Stanford <PStanford@janison.com.au>
2019-01-23 13:13:53 +1100

    lint: disable `strict-type-predicates` in is-object-tag.ts

--- src/types/type-guards/is-object-tag.ts
+++ src/types/type-guards/is-object-tag.ts
@@ -3,6 +3,8 @@ import { Constructor } from '../constructor';
 export function isObjectTag<T>(object: T, tag: string): boolean;
 export function isObjectTag<T>(object: T, constructor: Constructor<T>): boolean;
 export function isObjectTag<T>(object: T, tagOrConstructor: string | Constructor<T> & { name?: string }): boolean {
+    // TODO: Hopefully TSLint fixes it's problems with the `strict-type-predicates` rule
+    // tslint:disable-next-line:strict-type-predicates
     const name = typeof tagOrConstructor === 'function' ? tagOrConstructor.name : tagOrConstructor;
 
     return Object.prototype.toString.call(object) === `[object ${name}]`;
