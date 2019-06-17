a511e9cffb09e9a6bd81cddcd0b78f4141c9f582
Pathurs <pathurs123@gmail.com>
2019-06-17 17:10:08 +1000

    feat(copy): allow copy to accept multipe objects

--- source/objects/copy.ts
+++ source/objects/copy.ts
@@ -1,5 +1,7 @@
 import { copyFrom } from './copy-from';
 
-export function copy<T>(object: T): T {
-    return copyFrom(<T>{}, object);
+export function copy<T>(object: T): T;
+export function copy<T>(...objects: T[]): T;
+export function copy<T>(...objects: T[]): T {
+    return copyFrom(<T>{}, ...objects);
 }
