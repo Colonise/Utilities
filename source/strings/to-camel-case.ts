a553167470a0c566a6688c03d02817169e720659
Patrick Stanford <contact@pathurs.com>
2019-07-09 22:04:14 +1000

    feat(toCamelCase): add toCamelCase function

---
+++ source/strings/to-camel-case.ts
@@ -0,0 +1,22 @@
+import { copy } from '../objects';
+import { capitalise } from './capitalise';
+import { getWords, GetWordsOptions } from './get-words';
+import { toLowerCase } from './to-lower-case';
+
+export function toCamelCase(value: string): string;
+export function toCamelCase(value: string, options: GetWordsOptions): string;
+export function toCamelCase(value: string, options: GetWordsOptions = {}): string {
+    const words = getWords(value, copy({ pascalCaseAsSeparator: true }, options));
+
+    if (words.length === 0) {
+        return '';
+    }
+
+    let result = toLowerCase(words[0]);
+
+    for (let i = 1; i < words.length; i++) {
+        result += capitalise(words[i]);
+    }
+
+    return result;
+}
