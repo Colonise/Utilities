97a1157e47da857a13ee49504a4950f800df9b9e
Patrick Stanford <contact@pathurs.com>
2019-07-09 22:05:18 +1000

    feat(toDotCase): add toDotCase function

--- source/strings/index.ts
+++ source/strings/index.ts
@@ -1,5 +1,6 @@
 export * from './get-words';
 export * from './to-camel-case';
+export * from './to-dot-case';
 export * from './to-hyphen-case';
 export * from './to-lower-case';
 export * from './to-upper-case';
