284bfe258f945adbe137356d1874d13cad8f42eb
Patrick Stanford <contact@pathurs.com>
2020-02-20 09:04:07 +1100

    feat: remove extract in favour of optional chaining
    BREAKING CHANGE: extract function removed

--- source/objects/index.ts
+++ source/objects/index.ts
@@ -1,6 +1,5 @@
 export * from './clone';
 export * from './copy';
 export * from './copy-from';
-export * from './extract';
 export * from './get-own-property-descriptors';
 export * from './enumerate';
