35ebb08054fb09cc21e80c828f5c861a9a7be8c5
Patrick Stanford <contact@pathurs.com>
2023-07-06 17:12:25 +1000

    feat: Allow compareStrings and compareStringsBy to be case insensitive
    BREAKING CHANGE:
    Defaults compareStrings and compareStringsBy to case insenstive
    
    fixes #122

--- source/general/comparisons/compare-strings-by.ts
+++ source/general/comparisons/compare-strings-by.ts
@@ -2,7 +2,10 @@ import { compareBy } from './compare-by';
 import type { Comparer } from './comparer';
 import { compareStrings } from './compare-strings';
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-export function compareStringsBy<T>(valueGetter: (value: T) => string | null | undefined): Comparer<T> {
-    return compareBy(valueGetter, compareStrings);
+export function compareStringsBy<T>(
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    valueGetter: (value: T) => string | null | undefined,
+    caseInsensitive: boolean = true
+): Comparer<T> {
+    return compareBy(valueGetter, (itemA, itemB) => compareStrings(itemA, itemB, caseInsensitive));
 }
--- source/general/comparisons/compare-strings.ts
+++ source/general/comparisons/compare-strings.ts
@@ -4,19 +4,34 @@ export function compareStrings(
     // eslint-disable-next-line @typescript-eslint/ban-types
     itemA: string | null | undefined,
     // eslint-disable-next-line @typescript-eslint/ban-types
-    itemB: string | null | undefined
+    itemB: string | null | undefined,
+    caseInsensitive: boolean = true
 ): number {
     // eslint-disable-next-line no-eq-null, eqeqeq
     if (itemA == null || itemB == null) {
         return compareNullOrUndefined(itemA, itemB);
     }
 
-    if (itemA < itemB) {
-        return -1;
-    }
+    if (caseInsensitive) {
+        const itemALowercased = itemA.toLowerCase();
+        const itemBLowercased = itemB.toLowerCase();
 
-    if (itemA > itemB) {
-        return 1;
+        if (itemALowercased < itemBLowercased) {
+            return -1;
+        }
+
+        if (itemALowercased > itemBLowercased) {
+            return 1;
+        }
+    }
+    else {
+        if (itemA < itemB) {
+            return -1;
+        }
+
+        if (itemA > itemB) {
+            return 1;
+        }
     }
 
     return 0;
